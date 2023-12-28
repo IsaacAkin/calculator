ed431abfa3c81b634900aa95fda89607c69ad502
Halceeyon <IsaacAki247@gmail.com>
2023-12-28 00:42:51 +0000

    created a function to perform the calculator operations
    - created variables for containing created html elements.
    
    - created variables for storing the first number, operator and second number entered by the user
    
    - parsed the numbers entered into the add, subtract, multiply and division functions as float for accurate calculations
    
    - created the function "operate" that takes 3 parameters: the operator, the first number and the second number.
    The function calls one of the previous mathematical functions depending on the operator given and returns the result of the operation.

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
+"use strict";
+
+const display = document.querySelector(".display");
+const digits = document.querySelectorAll(".digit");
+const signs = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+
+let firstNumber;
+let operator;
+let secondNumber;
+
 function addition(num1, num2) {
-    return num1 + num2;
+    return parseFloat(num1) + parseFloat(num2);
 }
 
 function subtraction(num1, num2) {
-    return num1 - num2;
+    return parseFloat(num1) - parseFloat(num2);
 }
 
 function multiplication(num1, num2) {
-    return num1 * num2;
+    return parseFloat(num1) * parseFloat(num2);
 }
 
 function division(num1, num2) {
-    return num1 / num2;
+    return parseFloat(num1) / parseFloat(num2);
+}
+
+// Performs the calculation between numbers
+function operate(operator, firstNumber, secondNumber) {
+    if (operator === "+") {
+        return addition(firstNumber, secondNumber);
+    } else if (operator === "-") {
+        return subtraction(firstNumber, secondNumber);
+    } else if (operator === "*") {
+        return multiplication(firstNumber, secondNumber);
+    } else if (operator === "/") {
+        return division(firstNumber, secondNumber);
+    }
 }
